f872df289940c04043e68aa5b7169bd448e4a362
Ayaz Safin <ay.safin@innopolis.university>
2023-05-19 18:49:28 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,14 +3,40 @@ package main
 import (
 	"github.com/docker/go-plugins-helpers/volume"
 	"github.com/op/go-logging"
+	"os"
 )
 
-// TODO: set logging format
-var log = logging.MustGetLogger("docker-on-top")
+var log *logging.Logger = initLogger()
+
+func initLogger() *logging.Logger {
+	// Define the log format
+	logFormat := logging.MustStringFormatter(
+		"%{color}%{time:2006-01-02 15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x} %{message}",
+	)
+
+	// Create a log backend that writes to standard error
+	backend := logging.NewLogBackend(os.Stderr, "", 0)
+
+	// Apply the log format to the backend
+	backendFormatter := logging.NewBackendFormatter(backend, logFormat)
+
+	// Set the backend as the logging backend
+	logging.SetBackend(backendFormatter)
+
+	// Enable Debug level logs
+	logging.SetLevel(logging.DEBUG, "")
+
+	// Create and return the logger
+	return logging.MustGetLogger("docker-on-top")
+}
 
 func main() {
+
 	log.Info("Hello there!")
 
-	handler := volume.NewHandler(&Driver{volumesCreated: map[string]volume.Volume{}, overlaysCreated: map[string]string{}})
+	handler := volume.NewHandler(&Driver{
+		volumesCreated:  map[string]volume.Volume{},
+		overlaysCreated: map[string]string{},
+	})
 	log.Info(handler.ServeUnix("/run/docker/plugins/docker-on-top.sock", 0))
 }
