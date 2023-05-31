2b154ace047ff07f2aa5e441a85e0e4ac795f481
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2023-05-31 16:00:43 +0300

    Structured and improved internal directories handling
    Wrote a doc for which directories exist for volumes and how they are supposed to be managed. It will be used for storage implementation (#7)

--- driver.go
+++ driver.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"regexp"
 	"strings"
@@ -13,6 +14,17 @@ import (
 // The base directory of docker-on-top, where all the overlays' internals will be saved
 const dotBaseDir string = "/var/lib/docker-on-top/"
 
+func init() {
+	err := os.MkdirAll(dotBaseDir, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Failed to MkdirAll dotBaseDir: %v", dotBaseDir, err)
+	}
+}
+
+func internalError(help string, err error) error {
+	return fmt.Errorf("docker-on-top internal error: %s: %w", help, err)
+}
+
 type VolumeData struct {
 	Name        string
 	BaseDirPath string
@@ -20,21 +32,36 @@ type VolumeData struct {
 	MountsCount int // The number of containers using the volume at the moment
 }
 
+/*
+Here is how the internal volume management works.
+
+Each existing volume has a corresponding "main directory" named the same as the volume name located at the `dotBaseDir`
+directory. For example, if a volume with name FooBar is created, the main directory
+corresponding to it is /var/lib/docker-on-top/FooBar/ (as long as `dotBaseDir` is /var/lib/docker-on-top/).
+A volume's main directory is created when the volume is created and removed when the volume is removed.
+
+Inside a volume's main directory there are the following files/directories:
+	- upper/  - the upperdir of an overlay mount. Exists always. For volatile mounts, recreated from scratch on every
+		mount (unless the volume is already mounted to another container). On unmount no special action occurs.
+	- workdir/  - the workdir of an overlay mount. Exists only when the volume is mounted.
+	- mountpoint/  - the directory where the overlay is to be mounted to. Exists only when the volume is mounted.
+*/
+
 // Driver contains internal data for the docker-on-top volume driver. It does not export any fields
 type Driver struct {
 	volumes map[string]VolumeData
 }
 
-func (d *Driver) getMountpoint(volumeName string) string {
-	return dotBaseDir + d.volumes[volumeName].Name + "/mountpoint"
+func (d *Driver) mountpoint(volumeName string) string {
+	return dotBaseDir + volumeName + "/mountpoint"
 }
 
-func (d *Driver) getUpperdir(volumeName string) string {
-	return dotBaseDir + d.volumes[volumeName].Name + "/upper"
+func (d *Driver) upperdir(volumeName string) string {
+	return dotBaseDir + volumeName + "/upper"
 }
 
-func (d *Driver) getWorkdir(volumeName string) string {
-	return dotBaseDir + d.volumes[volumeName].Name + "/workdir"
+func (d *Driver) workdir(volumeName string) string {
+	return dotBaseDir + volumeName + "/workdir"
 }
 
 // This regex is based on the error message from docker daemon when requested to create a volume with invalid name
@@ -96,6 +123,26 @@ func (d *Driver) Create(request *volume.CreateRequest) error {
 		return errors.New("option `volatile` must be either 'true', 'false', 'yes', or 'no'")
 	}
 
+	if err := os.Mkdir(dotBaseDir+request.Name, os.ModePerm); err != nil {
+		if os.IsExist(err) {
+			log.Debug("Volume's main directory already exists. New volume not created")
+			return errors.New("volume already exists")
+		}
+		// Some other error - not expected
+		log.Errorf("Failed to Mkdir main directory: %v. Volume not created", err)
+		return internalError("failed to Mkdir volume main directory", err)
+	}
+
+	// Try to create upperdir. On failure, revert the creation of the volume main directory
+	if err := os.Mkdir(d.upperdir(request.Name), os.ModePerm); err != nil {
+		log.Errorf("Failed to Mkdir %s: %v. Removing the volume", request.Name, err)
+		cleanupErr := os.RemoveAll(dotBaseDir + request.Name)
+		if cleanupErr != nil {
+			log.Errorf("Failed to RemoveAll main directory: %v", dotBaseDir+request.Name, cleanupErr)
+		}
+		return internalError("failed to Mkdir upperdir", err)
+	}
+
 	d.volumes[request.Name] = VolumeData{Name: request.Name, BaseDirPath: baseDir, Volatile: volatile, MountsCount: 0}
 	return nil
 }
@@ -126,10 +173,11 @@ func (d *Driver) Get(request *volume.GetRequest) (*volume.GetResponse, error) {
 func (d *Driver) Remove(request *volume.RemoveRequest) error {
 	log.Debugf("Request Remove: Name=%s. It will succeed regardless of the presence of the volume", request.Name)
 	if _, ok := d.volumes[request.Name]; ok {
-		// Expecting the volume to have been unmounted by this moment
+		// Expecting the volume to have been unmounted by this moment. If it isn't, the error will be reported
 		err := os.RemoveAll(dotBaseDir + request.Name)
 		if err != nil {
-			return err
+			log.Errorf("Failed to RemoveAll main directory: %v", err)
+			return internalError("failed to RemoveAll volume main directory", err)
 		}
 	}
 	delete(d.volumes, request.Name)
@@ -138,7 +186,7 @@ func (d *Driver) Remove(request *volume.RemoveRequest) error {
 
 func (d *Driver) Path(request *volume.PathRequest) (*volume.PathResponse, error) {
 	log.Debugf("Request Path: Name=%s", request.Name)
-	return &volume.PathResponse{Mountpoint: d.getMountpoint(request.Name)}, nil
+	return &volume.PathResponse{Mountpoint: d.mountpoint(request.Name)}, nil
 }
 
 func (d *Driver) Mount(request *volume.MountRequest) (*volume.MountResponse, error) {
@@ -146,7 +194,7 @@ func (d *Driver) Mount(request *volume.MountRequest) (*volume.MountResponse, err
 
 	thisVol := d.volumes[request.Name] // Assuming the volume exists, as the docker daemon was supposed to check that
 
-	mountpoint := d.getMountpoint(request.Name)
+	mountpoint := d.mountpoint(request.Name)
 	response := volume.MountResponse{Mountpoint: mountpoint}
 
 	if thisVol.MountsCount > 0 {
@@ -159,15 +207,43 @@ func (d *Driver) Mount(request *volume.MountRequest) (*volume.MountResponse, err
 	}
 
 	lowerdir := d.volumes[request.Name].BaseDirPath
-	upperdir := d.getUpperdir(request.Name)
-	workdir := d.getWorkdir(request.Name)
+	upperdir := d.upperdir(request.Name)
+	workdir := d.workdir(request.Name)
 
-	for _, dir := range []string{lowerdir, upperdir, workdir, mountpoint} {
-		// TODO: create workdir with 000 permission
-		err := os.MkdirAll(dir, os.ModePerm)
+	err1 := os.Mkdir(mountpoint, os.ModePerm)
+	err2 := os.Mkdir(workdir, os.ModePerm)
+	err := errors.Join(err1, err2)
+	if err != nil {
+		log.Errorf("Failed to Mkdir mountpoint, workdir: %v, %v", err1, err2)
+
+		// Attempt to cleanup
+		if err1 == nil {
+			cleanupErr := os.Remove(mountpoint)
+			if cleanupErr != nil {
+				log.Errorf("Failed to cleanup mountpoint: %v", cleanupErr)
+			}
+		}
+		if err2 == nil {
+			cleanupErr := os.Remove(workdir)
+			if cleanupErr != nil {
+				log.Errorf("Failed to cleanup workdir: %v", cleanupErr)
+			}
+		}
+
+		return nil, internalError("failed to prepare internal directories", err)
+	}
+
+	// For volatile volume, discard previous changes
+	if thisVol.Volatile {
+		err := os.RemoveAll(upperdir)
 		if err != nil {
-			log.Errorf("Failed to create %s: %v", dir, err)
-			return nil, err
+			log.Errorf("Failed to RemoveAll upperdir (for volatile): %v", err)
+			return nil, internalError("failed to discard previous changes", err)
+		}
+		err = os.Mkdir(upperdir, os.ModePerm)
+		if err != nil {
+			log.Errorf("Failed to Mkdir upperdir (for volatile): %v", err)
+			return nil, internalError("failed to recreate upperdir", err)
 		}
 	}
 
@@ -175,7 +251,7 @@ func (d *Driver) Mount(request *volume.MountRequest) (*volume.MountResponse, err
 	// TODO: escape commas in directory names
 	data := "lowerdir=" + lowerdir + ",upperdir=" + upperdir + ",workdir=" + workdir
 
-	err := syscall.Mount("docker-on-top_"+request.Name, mountpoint, fstype, 0, data)
+	err = syscall.Mount("docker-on-top_"+request.Name, mountpoint, fstype, 0, data)
 	if err != nil {
 		log.Errorf("Failed to mount %s: %v", request.Name, err)
 		return nil, err
@@ -199,28 +275,27 @@ func (d *Driver) Unmount(request *volume.UnmountRequest) error {
 		return nil
 	}
 
-	err := syscall.Unmount(d.getMountpoint(request.Name), 0)
+	err := syscall.Unmount(d.mountpoint(request.Name), 0)
 	if err != nil {
 		log.Errorf("Failed to unmount %s: %v", request.Name, err)
 		return err
 	}
 
-	// For volatile volume, discard changes on last unmount
-	if thisVol.Volatile {
-		err1 := os.RemoveAll(d.getUpperdir(request.Name))
-		err2 := os.RemoveAll(d.getWorkdir(request.Name))
-		err = errors.Join(err1, err2)
-		if err != nil {
-			log.Errorf("Failed to remove upperdir,workdir for the volatile volume %s: %v", request.Name, err)
-		}
+	err1 := os.Remove(d.mountpoint(request.Name))
+	err2 := os.RemoveAll(d.workdir(request.Name))
+	err = errors.Join(err1, err2)
+	if err != nil {
+		log.Errorf("Cleanup of %s failed. Errors for mountpoint, workdir: %v, %v",
+			request.Name, err1, err2)
+		// Don't return yet. The error will be returned later
 	}
 
-	// Regardless of error with volatile cleanup, decrement the counter, as it tracks the number of active _mounts_,
+	// Regardless of whether cleanup succeeded, decrement the counter, as it tracks the number of active _mounts_,
 	// and the unmount system call was successful.
 	thisVol.MountsCount--
 	d.volumes[request.Name] = thisVol
 
-	// Report an error during volatility cleanup, if any, but most likely will just be `nil`
+	// Report an error during cleanup, if any, but most likely will just be `nil`
 	return err
 }
 
